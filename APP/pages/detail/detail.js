5110784e6b98b89d70ba6952e8ae5fcf862908b4
EricZhang809 <387991981@qq.com>
2019-06-24 16:01:42 +0800

    v1.1.0

--- APP/pages/detail/detail.js
+++ APP/pages/detail/detail.js
@@ -4,13 +4,30 @@ const app = getApp()
 
 Page({
   data: {
-    show:false
+    show:false,
+    goodsImg:'',
+    goods:'',
+    ratings:'',
+    sliderImg:[]
   },
   //事件处理函数
+ 
   bindViewTap: function () {
     
   },
-  onLoad: function () {
+  //请求接口数据
+  onLoad: function (options) {
+    wx.request({
+      url: 'http://www.puzhentec.com/www/api/public/index.php?s=api/v1/detail/72',
+      success: (res) => {
+        this.setData({
+          goodsImg: res.data.data.gallery,
+          goods: res.data.data.goods,
+          ratings: res.data.data.comment,
+          sliderImg: [res.data.data.goods.goods_thumb],
+        })
+      }
+    })
   },
   //打开详情页弹框
   onClickButton(){
